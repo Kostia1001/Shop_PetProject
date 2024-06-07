9a9b4c9d112ea0ebe998f422136e714c812a63ad
Kostia1001 <strong.ogurcov@gmail.com>
2024-06-07 14:13:24 +0300

    [+]added env

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store";
 
+const basename = process.env.PUBLIC_URL;
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={basename}>
     <Provider store={store}>
       <App />
     </Provider>
   </BrowserRouter>
 );
+
