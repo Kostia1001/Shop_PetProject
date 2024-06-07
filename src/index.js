e3dc87a13243ff5c30f2f86495bdd3af65190534
Kostia1001 <strong.ogurcov@gmail.com>
2024-06-07 14:31:41 +0300

    [*]

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store";
 
@@ -10,10 +10,10 @@ const basename = process.env.PUBLIC_URL;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter basename={basename}>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
 
